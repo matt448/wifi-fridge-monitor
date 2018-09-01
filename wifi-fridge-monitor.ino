a97afed11f788fa6a9cc38605fa816bc163d402d
Matthew McMillan <matthew.mcmillan@gmail.com>
2018-08-31 22:59:54 -0500

    Wifi connects to ap and sends request to mock api

---
+++ wifi-fridge-monitor.ino
@@ -0,0 +1,115 @@
+#include <SPI.h>
+#include <WiFi101.h>
+
+#include "secrets.h"
+// Copy example_secrets.h to secrets.h
+// Then enter ssid and password secrets.h
+char ssid[] = SECRET_SSID;        // wifi ssid
+char pass[] = SECRET_PASS;       // wifi network password (use for WPA, or use as key for WEP)
+int status = WL_IDLE_STATUS;     // the WiFi radio's status
+
+WiFiClient client;
+
+char server[] = "mywififridge.free.beeceptor.com";
+
+void setup() {
+  WiFi.setPins(8,7,4,2);
+
+  Serial.begin(115200);
+  while (!Serial) {
+    ; // wait for serial port to connect. Needed for native USB port only
+  }
+
+  // check for the presence of the shield:
+  if (WiFi.status() == WL_NO_SHIELD) {
+    Serial.println("WiFi shield not present");
+    // don't continue:
+    while (true);
+  }
+
+  // attempt to connect to WiFi network:
+  while (status != WL_CONNECTED) {
+    Serial.print("Attempting to connect to WPA SSID: ");
+    Serial.println(ssid);
+    // Connect to WPA/WPA2 network:
+    status = WiFi.begin(ssid, pass);
+
+    // wait 10 seconds for connection:
+    delay(10000);
+  }
+
+  Serial.println("Connected to wifi");
+  printWiFiStatus();
+  connectToServer();
+ 
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  //delay(5000);
+  //printWifiRSSI();
+  
+  // if there are incoming bytes available
+  // from the server, read them and print them:
+  while (client.available()) {
+    char c = client.read();
+    Serial.write(c);
+  }
+
+  // if the server's disconnected, stop the client:
+  if (!client.connected()) {
+    Serial.println();
+    Serial.println("disconnecting from server.");
+    client.stop();
+
+    // do nothing forevermore:
+    while (true);
+  }
+}
+
+
+void printWiFiStatus() {
+  // print the SSID of the network you're attached to:
+  Serial.print("SSID: ");
+  Serial.println(WiFi.SSID());
+
+  // print your WiFi shield's IP address:
+  IPAddress ip = WiFi.localIP();
+  Serial.print("IP Address: ");
+  Serial.println(ip);
+
+  // print the received signal strength:
+  long rssi = WiFi.RSSI();
+  Serial.print("signal strength (RSSI):");
+  Serial.print(rssi);
+  Serial.println(" dBm");
+}
+
+void printWifiRSSI() {
+  // print the received signal strength:
+  long rssi = WiFi.RSSI();
+  Serial.print("signal strength (RSSI):");
+  Serial.print(rssi);
+  Serial.println(" dBm");
+}
+
+void connectToServer() {
+  String  data = "{\"foo\" : \"baz\"}";
+  //String  data = "foo=baz";
+  Serial.println("\nStarting connection to server...");
+  // if you get a connection, report back via serial:
+  if (client.connect(server, 80)) {
+    Serial.println("connected to server");
+    // Make a HTTP request:
+    client.println("POST /api HTTP/1.1");
+    client.println("Host: mywififridge.free.beeceptor.com");
+    client.println("Content-Type: application/json");
+    client.print("Content-Length: ");
+    client.println(data.length());
+    client.println();
+    client.print(data);
+    client.println("Connection: close");
+    client.println();
+  }
+  
+}
